43399e58267523aa794b1c03950590e56bc34459
Arthur Lage <arthurlage2006@gmail.com>
2025-11-08 11:41:21 -0300

    feat: create progress model

--- src/main/java/com/arthurlage/habitflow/model/Progress.java
+++ src/main/java/com/arthurlage/habitflow/model/Progress.java
@@ -1,4 +1,20 @@
 package com.arthurlage.habitflow.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
 public class Progress {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne @JoinColumn(name = "habit_id")
+    private Habit habit;
+
+    private LocalDate date;
+    private boolean completed;
+
 }
