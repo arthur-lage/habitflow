883e5f3a5fe5fb602bf2fb3cfee5cf7850520718
Arthur Lage <arthurlage2006@gmail.com>
2025-11-08 11:42:57 -0300

    feat: add security configuration bean to disable spring security login page

---
+++ src/main/java/com/arthurlage/habitflow/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.arthurlage.habitflow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+                return httpSecurity.build();
+    }
+}
