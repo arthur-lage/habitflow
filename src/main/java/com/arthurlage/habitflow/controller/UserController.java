b4e91d845c485eca57c0563ef1befaa01aa338ac
Arthur Lage <arthurlage2006@gmail.com>
2025-11-09 18:33:26 -0300

    feat: create get and create user endpoints

--- src/main/java/com/arthurlage/habitflow/controller/UserController.java
+++ src/main/java/com/arthurlage/habitflow/controller/UserController.java
@@ -1,14 +1,32 @@
 package com.arthurlage.habitflow.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.arthurlage.habitflow.dto.CreateUserRequestDTO;
+import com.arthurlage.habitflow.dto.CreateUserResponseDTO;
+import com.arthurlage.habitflow.model.User;
+import com.arthurlage.habitflow.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/api/users")
 @RestController
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
     @GetMapping
-    public String getUser() {
-        return "Hello user";
+    public List<User> getUsers() {
+        return this.userService.getUsers();
+    }
+
+    @PostMapping
+    public ResponseEntity<CreateUserResponseDTO> createUser (@RequestBody CreateUserRequestDTO data) {
+        this.userService.createUser(data);
+        CreateUserResponseDTO body = new CreateUserResponseDTO("User created successfully");
+        return ResponseEntity.ok(body);
     }
 }
