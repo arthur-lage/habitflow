49eef94ab000fe6230ee76b5308a90c799f82ba6
Arthur Lage <arthurlage2006@gmail.com>
2025-11-09 18:31:30 -0300

    feat: create global exception handler

---
+++ src/main/java/com/arthurlage/habitflow/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.arthurlage.habitflow.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(UsernameOrEmailTakenException.class)
+    public ResponseEntity<Map<String, String>> handleUserExists(UsernameOrEmailTakenException ex) {
+        Map<String, String> body = new HashMap<>();
+        body.put("error", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(body);
+    }
+}
