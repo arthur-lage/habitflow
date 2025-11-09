b49447a2fc54ef6964b45901a9d91b6ceeeadd8a
Arthur Lage <arthurlage2006@gmail.com>
2025-11-09 18:33:44 -0300

    feat: create create user logic for user service

--- src/main/java/com/arthurlage/habitflow/service/UserService.java
+++ src/main/java/com/arthurlage/habitflow/service/UserService.java
@@ -1,5 +1,7 @@
 package com.arthurlage.habitflow.service;
 
+import com.arthurlage.habitflow.dto.CreateUserRequestDTO;
+import com.arthurlage.habitflow.exception.UsernameOrEmailTakenException;
 import com.arthurlage.habitflow.model.User;
 import com.arthurlage.habitflow.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -16,5 +18,20 @@ public class UserService {
 
     public List<User> getUsers () {
         return this.userRepository.findAll();
+    }
+
+    public void createUser(CreateUserRequestDTO data) {
+
+        if(this.userRepository.existsByUsername(data.username())) {
+            throw new UsernameOrEmailTakenException("This username is already being used.");
+        }
+
+        if(this.userRepository.existsByEmail(data.email())) {
+            throw new UsernameOrEmailTakenException("This email is already being used.");
+        }
+
+        User user = new User(data);
+
+        this.userRepository.save(user);
     }
 }
