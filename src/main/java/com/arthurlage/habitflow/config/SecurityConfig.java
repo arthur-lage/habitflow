faedb3651033f0e3c2cb62ec29e90b90e677286b
Arthur Lage <arthurlage2006@gmail.com>
2025-11-14 14:53:26 -0300

    feat: add bcrypt encoder as bean to security config

--- src/main/java/com/arthurlage/habitflow/config/SecurityConfig.java
+++ src/main/java/com/arthurlage/habitflow/config/SecurityConfig.java
@@ -4,10 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
