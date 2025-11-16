a624d97b0ea032b0320c8cf8b13d417b451c81c4
Arthur Lage <arthurlage2006@gmail.com>
2025-11-16 16:34:57 -0300

    feat: create custom user details for jwt auth

---
+++ src/main/java/com/arthurlage/habitflow/security/CustomUserDetails.java
@@ -0,0 +1,50 @@
+package com.arthurlage.habitflow.security;
+
+import com.arthurlage.habitflow.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // Se o seu User não tem role, só dá um ROLE_USER pra tudo funcionar
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    public User getUser () {
+        return user;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
+}
---
+++ src/main/java/com/arthurlage/habitflow/service/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.arthurlage.habitflow.service;
+
+import com.arthurlage.habitflow.model.User;
+import com.arthurlage.habitflow.repository.UserRepository;
+import com.arthurlage.habitflow.security.CustomUserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        return new CustomUserDetails(user);
+
+    }
+}
