ccc91aef5344e9e637de4b23754abd7e36081f38
Arthur Lage <arthurlage2006@gmail.com>
2025-11-16 16:34:08 -0300

    feat: create find by user id query for habit repo

--- src/main/java/com/arthurlage/habitflow/repository/HabitRepository.java
+++ src/main/java/com/arthurlage/habitflow/repository/HabitRepository.java
@@ -4,6 +4,9 @@ import com.arthurlage.habitflow.model.Habit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HabitRepository extends JpaRepository<Habit, Long> {
+    List<Habit> findByUserId(Long userId);
 }
