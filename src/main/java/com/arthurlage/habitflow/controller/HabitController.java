8bf07f4b9e86020871248f7ad4b7c7ff3b3532f3
Arthur Lage <arthurlage2006@gmail.com>
2025-11-16 16:34:44 -0300

    feat: add habit creation methods

--- src/main/java/com/arthurlage/habitflow/controller/HabitController.java
+++ src/main/java/com/arthurlage/habitflow/controller/HabitController.java
@@ -1,14 +1,37 @@
 package com.arthurlage.habitflow.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.arthurlage.habitflow.dto.CreateHabitRequestDTO;
+import com.arthurlage.habitflow.dto.CreateHabitResponseDTO;
+import com.arthurlage.habitflow.model.Habit;
+import com.arthurlage.habitflow.model.User;
+import com.arthurlage.habitflow.security.CustomUserDetails;
+import com.arthurlage.habitflow.service.HabitService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/habits")
 public class HabitController {
+    private final HabitService habitService;
+
+    public HabitController(HabitService habitService) {
+        this.habitService = habitService;
+    }
+
     @GetMapping
-    public String getHabits() {
-        return "Hello";
+    public List<Habit> getHabits(@AuthenticationPrincipal User user) {
+        return this.habitService.getHabits(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<CreateHabitResponseDTO> createHabit(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestBody @Valid CreateHabitRequestDTO createHabitDTO) {
+        User user = userDetails.getUser();
+        Habit habit = this.habitService.createHabit(createHabitDTO, user);
+        CreateHabitResponseDTO body = new CreateHabitResponseDTO(habit, "Habit was created successfully.");
+        return ResponseEntity.ok(body);
     }
 }
--- src/main/java/com/arthurlage/habitflow/service/HabitService.java
+++ src/main/java/com/arthurlage/habitflow/service/HabitService.java
@@ -1,6 +1,8 @@
 package com.arthurlage.habitflow.service;
 
+import com.arthurlage.habitflow.dto.CreateHabitRequestDTO;
 import com.arthurlage.habitflow.model.Habit;
+import com.arthurlage.habitflow.model.User;
 import com.arthurlage.habitflow.repository.HabitRepository;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +16,12 @@ public class HabitService {
         this.habitRepository = habitRepository;
     }
 
-    public List<Habit> getHabits () {
-        return this.habitRepository.findAll();
+    public List<Habit> getHabits (User user) {
+        return this.habitRepository.findByUserId(user.getId());
+    }
+
+    public Habit createHabit(CreateHabitRequestDTO createHabitRequestDTO, User user) {
+        Habit habit = new Habit(createHabitRequestDTO, user);
+        return this.habitRepository.save(habit);
     }
 }
